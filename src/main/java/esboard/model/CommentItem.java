b359a21975c9e925dfd42f9fb59f57200f715ed2
nct04 <kimgs2024@gmail.com>
2026-03-11 16:59:45 +0900

    fourth commit : elasticsearch board project

--- src/main/java/esboard/model/CommentItem.java
+++ src/main/java/esboard/model/CommentItem.java
@@ -13,12 +13,12 @@ import java.time.LocalDateTime;
 @Document(indexName = "board_comments")
 public class CommentItem {
     @Id
-    private String id;          // 댓글 고유 ID
+    private String id;
 
     @Field
-    private String boardId;     // ⭐ 중요: 연결된 게시글 ID
-    private String author;      // 댓글 작성자
-    private String content;     // 댓글 내용
+    private String boardId;
+    private String author;
+    private String content;
 
     @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second_millis)
     private LocalDateTime createdAt;
